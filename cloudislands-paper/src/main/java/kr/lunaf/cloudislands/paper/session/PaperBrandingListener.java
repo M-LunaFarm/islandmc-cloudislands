72e8238be7f66b79c82738c4498be63185915641
LeeSeungmin <admin@lunaf.kr>
2026-06-12 09:54:00 +0900

    Unify paper join messages

--- cloudislands-paper/src/main/java/kr/lunaf/cloudislands/paper/session/PaperBrandingListener.java
+++ cloudislands-paper/src/main/java/kr/lunaf/cloudislands/paper/session/PaperBrandingListener.java
@@ -4,6 +4,7 @@ import net.kyori.adventure.text.Component;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 public final class PaperBrandingListener implements Listener {
     private final Component header;
@@ -18,5 +19,11 @@ public final class PaperBrandingListener implements Listener {
     @EventHandler
     public void onJoin(PlayerJoinEvent event) {
         event.getPlayer().sendPlayerListHeaderAndFooter(header, footer);
+        event.joinMessage(Component.text(event.getPlayer().getName() + "님이 섬 서비스에 접속했습니다."));
+    }
+
+    @EventHandler
+    public void onQuit(PlayerQuitEvent event) {
+        event.quitMessage(Component.text(event.getPlayer().getName() + "님이 섬 서비스에서 나갔습니다."));
     }
 }
