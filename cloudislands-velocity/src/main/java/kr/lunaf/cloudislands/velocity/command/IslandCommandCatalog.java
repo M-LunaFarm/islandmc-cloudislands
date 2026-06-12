520e478cae62bd1f2eb518406a64a121d3bba0c4
LeeSeungmin <admin@lunaf.kr>
2026-06-12 15:35:52 +0900

    Show block value args in catalog

--- cloudislands-velocity/src/main/java/kr/lunaf/cloudislands/velocity/command/IslandCommandCatalog.java
+++ cloudislands-velocity/src/main/java/kr/lunaf/cloudislands/velocity/command/IslandCommandCatalog.java
@@ -35,7 +35,7 @@ public final class IslandCommandCatalog {
             "ciadmin node sweep", "ciadmin node shutdown-safe", "ciadmin jobs list", "ciadmin jobs retry",
             "ciadmin jobs cancel", "ciadmin jobs recover", "ciadmin route debug [all|player]", "ciadmin route ticket <ticket|player>",
             "ciadmin route clear <player> [ticket]", "ciadmin cache clear", "ciadmin events", "ciadmin audit",
-            "ciadmin block-values list", "ciadmin block-values set", "ciadmin upgrade-rules", "ciadmin template list",
+            "ciadmin block-values list", "ciadmin block-values set <materialKey> <worth> <levelPoints> <limit>", "ciadmin upgrade-rules", "ciadmin template list",
             "ciadmin template upsert <id> <name> [enabled] [minNodeVersion]", "ciadmin template enable <id>",
             "ciadmin template disable <id>", "ciadmin templates list",
             "ciadmin templates upsert <id> <name> [enabled] [minNodeVersion]", "ciadmin templates enable <id>",
