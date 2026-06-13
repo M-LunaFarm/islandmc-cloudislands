685caf12d940184540ac05ea93e6255575052cdc
LeeSeungmin <admin@lunaf.kr>
2026-06-13 11:32:17 +0900

    Expose permission cache metrics

--- cloudislands-paper/src/main/java/kr/lunaf/cloudislands/paper/CloudIslandsPaperPlugin.java
+++ cloudislands-paper/src/main/java/kr/lunaf/cloudislands/paper/CloudIslandsPaperPlugin.java
@@ -367,6 +367,8 @@ public final class CloudIslandsPaperPlugin extends JavaPlugin {
             + "cloudislands_island_save_seconds{node=\"" + nodeId + "\"} " + storageUploadSeconds + "\n"
             + "cloudislands_island_activation_seconds{node=\"" + nodeId + "\"} " + storageDownloadSeconds + "\n"
             + "cloudislands_island_snapshot_seconds{node=\"" + nodeId + "\"} " + storageUploadSeconds + "\n"
+            + "cloudislands_permission_checks_total{node=\"" + nodeId + "\"} " + agent.permissionCache().lookupCount() + "\n"
+            + "cloudislands_permission_cache_hit_ratio{node=\"" + nodeId + "\"} " + agent.permissionCache().hitRatio() + "\n"
             + "cloudislands_redis_latency_seconds{node=\"" + nodeId + "\"} " + redis.latencySeconds() + "\n"
             + "cloudislands_paper_redis_available{node=\"" + nodeId + "\"} " + (redis.available() ? 1 : 0) + "\n"
             + "cloudislands_paper_redis_latency_seconds{node=\"" + nodeId + "\"} " + redis.latencySeconds() + "\n"
