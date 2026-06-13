558fbebc9fcd1534e164d7c2d7040f03ced400d8
LeeSeungmin <admin@lunaf.kr>
2026-06-14 03:56:52 +0900

    Isolate dirty save test path

--- cloudislands-satis/src/test/java/kr/seungmin/satisskyfactory/task/DirtySaveServiceTest.java
+++ cloudislands-satis/src/test/java/kr/seungmin/satisskyfactory/task/DirtySaveServiceTest.java
@@ -2,16 +2,21 @@ package kr.seungmin.satisskyfactory.task;
 
 import kr.seungmin.satisskyfactory.database.DatabaseService;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 
 import java.io.File;
+import java.nio.file.Path;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 class DirtySaveServiceTest {
+    @TempDir
+    Path tempDir;
+
     @Test
     void ignoresNullQueueInputs() {
-        DirtySaveService service = new DirtySaveService(null, new DatabaseService(new File("unused")));
+        DirtySaveService service = new DirtySaveService(null, new DatabaseService(tempDir.resolve("unused").toFile()));
 
         assertDoesNotThrow(() -> service.markMachine(null));
         assertDoesNotThrow(() -> service.forgetMachine(null));
