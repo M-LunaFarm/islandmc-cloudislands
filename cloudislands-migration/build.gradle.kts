01fd16ba9940427600cdf3df1cb17e328d3a3689
LeeSeungmin <admin@lunaf.kr>
2026-06-15 22:46:28 +0900

    Mark migration jar input only

--- cloudislands-migration/build.gradle.kts
+++ cloudislands-migration/build.gradle.kts
@@ -5,3 +5,12 @@ dependencies {
     implementation(project(":cloudislands-storage"))
     compileOnly("org.postgresql:postgresql:42.7.7")
 }
+
+tasks.jar {
+    manifest {
+        attributes(
+            "SuperiorSkyblock2-Migration-Input-Only" to "true",
+            "SuperiorSkyblock2-Runtime-Dependency" to "false"
+        )
+    }
+}
